537813e141ebe625452dc3538ed979a74eb5f451
Michael A <nomicflux@gmail.com>
2016-08-29 21:42:39 -0500

    Updated to Servant 0.5

--- src/App.hs
+++ src/App.hs
@@ -1,10 +1,10 @@
 module App where
 
 import Data.Int (Int64)
-import Control.Monad.Trans.Either (EitherT)
+import Control.Monad.Trans.Except (ExceptT)
 import Servant (ServantErr)
 
 type BlogPostID = Int64
 type Email = String
 
-type AppM = EitherT ServantErr IO
+type AppM = ExceptT ServantErr IO
